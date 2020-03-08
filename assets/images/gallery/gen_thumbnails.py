8a2d4a1ce5f2b64498befe9bae30bc7a617f9287
Alex <17598972+sandpipes@users.noreply.github.com>
2020-03-07 21:45:55 -0500

    more fanart & official images

--- assets/images/gallery/gen_thumbnails.py
+++ assets/images/gallery/gen_thumbnails.py
@@ -1,14 +1,22 @@
 from PIL import Image
-import os
+import os, sys
 from os.path import isfile, join
 
 directories = ['fanart', 'official']
+prev = 0
 
 for directory in directories:
     onlyfiles = [f for f in os.listdir(directory) if isfile(join(directory, f))]
+    print('Begin work on ' + directory + ' folder.')
 
     for filename in onlyfiles:
         if '.jpg' in filename.lower() or '.png' in filename.lower():
+            ost = '\rWorking on ' + filename
+            sys.stdout.write('\r' + prev * ' ')
+            sys.stdout.flush()
+            sys.stdout.write(ost)
+            sys.stdout.flush()
+            prev = len(ost)
             orig = Image.open(join(directory, filename))
             img = None
             if orig.mode in ('RGBA', 'LA') or (orig.mode == 'P' and 'transparency' in orig.info):
@@ -26,4 +34,4 @@ for directory in directories:
             if not os.path.exists("./thumbnails/" + directory):
                 os.makedirs("./thumbnails/" + directory)
             img.save(join("./thumbnails/" + directory, filename.replace('png', 'jpg')), optimize=True, quality=50)
-
+    print('\n')
